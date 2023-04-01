c11071fedfd23ac4a727d6f08e250734eecd5685
You-now-Who <yashyadav.sr@gmail.com>
2023-04-02 03:50:29 +0530

    Added Authentication

--- boutik/src/App.js
+++ boutik/src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Nabar from './components/Navbar';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import Home from './components/Home';
+import {BrowserRouter, Route, Routes, Link} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nabar />
+      
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" >
+          <Route path='home' element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+
+        </Route>
+      </Routes>
+      </BrowserRouter>
+
+      {/* <Login /> */}
+      {/* <Register /> */}
+    </>
   );
 }
 
---
+++ boutik/src/components/auth/Register.js
@@ -0,0 +1,122 @@
+import React, {useRef, useState} from 'react'
+import { auth } from "../../firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+
+import { Link } from "react-router-dom"
+
+export default function Register() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirm, setPasswordConfirm] = useState("");
+
+  const signUp = (e) => {
+    e.preventDefault();
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        console.log(userCredential);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  
+
+
+  
+  return (
+    <>
+        <section className="h-screen">
+  <div className="container h-full px-6 py-12">
+    <div
+      className="g-6 flex h-full flex-wrap items-center justify-center lg:justify-between">
+      <div className="mb-12 md:mb-0 md:w-8/12 lg:w-6/12">
+        <img
+          src="https://i.pinimg.com/originals/1e/79/38/1e7938069bebbd950a8c633f99321085.jpg"
+          className="w-full"
+          alt="Phone image" />
+      </div>
+      <div className="md:w-8/12 lg:ml-6 lg:w-5/12">
+        <form onSubmit={signUp}>
+            <h1 className='text-3xl font-bold my-5' >Register</h1>
+          {/* <!-- Email input --> */}
+          <div className="relative mb-6 outline" data-te-input-wrapper-init>
+            <input
+              type="text"
+              className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear "
+              id="exampleFormControlInput3"
+              placeholder="Email address" 
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required/>
+            {/* <label
+              for="exampleFormControlInput3"
+              className="pointer-events-none absolute top-0 left-3 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-800 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[1.15rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-600 dark:peer-focus:text-neutral-500"
+              >Email address
+            </label> */}
+          </div>
+
+          {/* <!-- Password input --> */}
+          <div className="relative mb-6 outline" data-te-input-wrapper-init>
+            <input
+              type="password"
+              className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear"
+              id="exampleFormControlInput33"
+              placeholder="Password" 
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required/>
+              
+          </div>
+          
+          {/* <!-- Confirm Password input --> */}
+          <div className="relative mb-6 outline" data-te-input-wrapper-init>
+            <input
+              type="password"
+              className="peer block min-h-[auto] w-full rounded border-0 bg-transparent py-[0.32rem] px-3 leading-[2.15] outline-none transition-all duration-200 ease-linear"
+              id="exampleFormControlInput34"
+              placeholder="Confirm Password" 
+              value={passwordConfirm}
+              onChange={(e) => setPasswordConfirm(e.target.value)}
+              required/>
+          </div>
+
+          <div className="mb-6 flex items-center justify-between">
+            <div className="mb-[0.125rem] block min-h-[1.5rem] pl-[1.5rem]">
+              <input
+                className="relative float-left mt-[0.15rem] mr-[6px] -ml-[1.5rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-neutral-300 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-[0px_0px_0px_13px_transparent] before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:ml-[0.25rem] checked:after:-mt-px checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-t-0 checked:after:border-l-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[0px_0px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-[0px_0px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:ml-[0.25rem] checked:focus:after:-mt-px checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-t-0 checked:focus:after:border-l-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent dark:border-neutral-600 dark:checked:border-primary dark:checked:bg-primary"
+                type="checkbox"
+                value=""
+                id="exampleCheck3"
+                checked />
+              <label
+                className="inline-block pl-[0.15rem] hover:cursor-pointer"
+                for="exampleCheck3">
+                Remember me
+              </label>
+            </div>
+            <a
+              href="#!"
+              className="text-primary transition duration-150 ease-in-out hover:text-primary-600 focus:text-primary-600 active:text-primary-700 dark:text-cyan-900 dark:hover:text-cyan-700 dark:focus:text-cyan-500 dark:active:text-cyan-600"
+              >Already a user? Sign in.</a
+            >
+          </div>
+
+          {/* <!-- Submit button --> */}
+          <button
+            type="submit"
+            className="inline-block w-full bg-gray-800 text-white px-8 py-3 text-sm font-medium uppercase leading-normal border-2 border-gray-800 transition duration-150 ease-in-out hover:bg-gray-900 hover:border-gray-900 focus:outline-none focus:ring-2 focus:ring-gray-800 focus:ring-opacity-50 active:bg-gray-700 active:border-gray-700 active:shadow-none"
+            data-te-ripple-init
+            data-te-ripple-color="light"
+          >
+            Register
+          </button>
+
+          
+        </form>
+      </div>
+    </div>
+  </div>
+</section>
+    </>
+  )
+}
